55a7a8907e4ec5618d2ce3fb9d84221a25754c11
dylancdavis <dylcdav@gmail.com>
2023-10-20 14:25:52 -0400

    Add collection to constants

--- src/deckbuilder/db.cljs
+++ src/deckbuilder/db.cljs
@@ -1,6 +1,8 @@
 (ns deckbuilder.db
-  (:require [deckbuilder.game.cards :as cards]))
+  (:require [deckbuilder.constants :as constants]
+            [deckbuilder.game.cards :as cards]))
 
 (def default-db
   {:round cards/starting-round
-   :resources {:energy {:display "Energy" :value 0} :credits {:display "Credits" :value 0}}})
+   :resources {:energy {:display "Energy" :value 0} :credits {:display "Credits" :value 0}}
+   :collection {:cards {} :card-backs {} :decklists [constants/starting-deck-list]}})
