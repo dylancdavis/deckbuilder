4208f6cf4833e18ff5ea3d1c3ac89f47d9e7a428
dylancdavis <dylcdav@gmail.com>
2023-10-20 14:26:37 -0400

    Remove missing card from decklist

--- src/deckbuilder/constants.cljs
+++ src/deckbuilder/constants.cljs
@@ -1,4 +1,4 @@
 (ns deckbuilder.constants
   (:require [deckbuilder.game.cards :as cards]))
 
-(def starting-deck-list (concat [cards/draft] (take 7 (repeat cards/energy)) (take 3 (repeat cards/credit-generator))))
+(def starting-deck-list (concat (take 7 (repeat cards/energy)) (take 3 (repeat cards/credit-generator))))
