184db69a742afb6c94b12e9872dce5ed459cc792
dylancdavis <dylcdav@gmail.com>
2023-11-19 19:54:11 -0500

    Add rules card to starting deck list

--- src/deckbuilder/constants.cljs
+++ src/deckbuilder/constants.cljs
@@ -3,7 +3,7 @@
 
 ;; (def starting-deck-list {:name "Starter Deck" :cardlist (concat (take 7 (repeat cards/energy)) (take 3 (repeat cards/credit-generator)))})
 
-(def starting-deck-list {:name "Starter Deck" :cards {cards/energy 7 cards/credit-generator 2 cards/buy-basic 1}})
+(def starting-deck-list {:name "Starter Deck" :cards {cards/energy 7 cards/credit-generator 2 cards/buy-basic 1} :rules-card cards/basic-rules})
 
 (def starting-round-resources {:energy 0 :credits 0})
 
