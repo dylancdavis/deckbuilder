4d35a2b1ff20eaa71f83e76e0c278ca48d308947
dylancdavis <dylcdav@gmail.com>
2023-12-19 17:41:42 -0500

    Convert SVG to react component

---
+++ resources/public/svg/lightning.js
@@ -0,0 +1,30 @@
+import * as React from "react"
+const SvgComponent = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    width={124.581}
+    height={174.258}
+    viewBox="172.581 59.677 124.581 174.258"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      d="M32.903-86.129h-69.677L-61.29 4.194h49.032l-15.484 81.935L61.29-20.323H9.032z"
+      style={{
+        stroke: "#000",
+        strokeWidth: 2,
+        strokeDasharray: "none",
+        strokeLinecap: "butt",
+        strokeDashoffset: 0,
+        strokeLinejoin: "miter",
+        strokeMiterlimit: 4,
+        fill: "#fff",
+        fillRule: "nonzero",
+        opacity: 1,
+      }}
+      transform="translate(234.871 146.806)"
+    />
+  </svg>
+)
+export default SvgComponent
