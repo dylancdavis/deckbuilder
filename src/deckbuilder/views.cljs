dfd2ee7c66635bb9b65b786c9b03162ab450e742
dylancdavis <dylcdav@gmail.com>
2023-12-19 19:29:53 -0500

    Update lightning-svg component

--- src/deckbuilder/views.cljs
+++ src/deckbuilder/views.cljs
@@ -5,20 +5,26 @@
    [deckbuilder.game.cards :as cards]))
 
 (def lightning-svg
-  [:svg [:path
-         {:style {:stroke "#000"
-                  :strokeWidth 2
-                  :strokeDasharray "none"
-                  :strokeLinecap "butt"
-                  :strokeDashoffset 0
-                  :strokeLinejoin "miter"
-                  :strokeMiterlimit 4
-                  :fill "#fff"
-                  :fillRule "nonzero"
-                  :opacity 1}
-          :transform "translate(-0.000005, 0)"
-          :d "M 32.90323 -86.12903 L -36.77419 -86.12903 L -61.290319999999994 4.193550000000002 L -12.258059999999993 4.193550000000002 L -27.741929999999993 86.12903 L 61.29033 -20.322580000000002 L 9.032269999999997 -20.322580000000002 z"
-          :stroke-linecap "round"}]])
+  [:svg
+   {:xmlns "http://www.w3.org/2000/svg"
+    :xmlSpace "preserve"
+    :width 125
+    :height 175
+    :view-box "-1 -1.4 127 177.8"}
+   [:path
+    {:strokeLinecap "round"
+     :d "M32.903-86.129h-69.677L-61.29 4.194h49.032L-27.74 86.129 61.29-20.323H9.032z"
+     :style {:stroke "#000",
+             :strokeWidth 2,
+             :strokeDasharray "none",
+             :strokeLinecap "butt",
+             :strokeDashoffset 0,
+             :strokeLinejoin "miter",
+             :strokeMiterlimit 4,
+             :fill "#fff",
+             :fillRule "nonzero",
+             :opacity 1}
+     :transform "matrix(1.01974 0 0 1.01592 62.5 87.5)"}]])
 
 (defn card-item [card]
   [:div.card-container {:key (js/Math.random)}
