e3995e3aa390c7e1609e63bf2ff204bae99862a6
dylancdavis <dylcdav@gmail.com>
2023-12-19 19:24:41 -0500

    Update lightning.js

--- resources/public/svg/lightning.js
+++ resources/public/svg/lightning.js
@@ -1,16 +1,16 @@
-import * as React from "react"
+import * as React from "react";
 const SvgComponent = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlSpace="preserve"
-    width={124.581}
-    height={174.258}
-    viewBox="172.581 59.677 124.581 174.258"
+    width={125}
+    height={175}
+    viewBox="-1 -1.4 127 177.8"
     {...props}
   >
     <path
       strokeLinecap="round"
-      d="M32.903-86.129h-69.677L-61.29 4.194h49.032l-15.484 81.935L61.29-20.323H9.032z"
+      d="M32.903-86.129h-69.677L-61.29 4.194h49.032L-27.74 86.129 61.29-20.323H9.032z"
       style={{
         stroke: "#000",
         strokeWidth: 2,
@@ -23,8 +23,8 @@ const SvgComponent = (props) => (
         fillRule: "nonzero",
         opacity: 1,
       }}
-      transform="translate(234.871 146.806)"
+      transform="matrix(1.01974 0 0 1.01592 62.5 87.5)"
     />
   </svg>
-)
-export default SvgComponent
+);
+export default SvgComponent;
