103c9ab779dfaa426897d8d5d61cca1d373b9091
dylancdavis <dylcdav@gmail.com>
2023-12-21 12:48:27 -0500

    Add cards to starting db for testing

--- src/deckbuilder/db.cljs
+++ src/deckbuilder/db.cljs
@@ -1,7 +1,8 @@
 (ns deckbuilder.db
-  (:require [deckbuilder.constants :as constants]))
+  (:require [deckbuilder.constants :as constants])
+  (:require [deckbuilder.game.cards :as cards]))
 
 (def default-db
   {:view :collection
-   :collection {:cards {} :card-backs {} :decklists {:starting-deck constants/starting-deck-list}}
+   :collection {:cards {cards/energy 12 cards/basic-rules 7} :card-backs {} :decklists {:starting-deck constants/starting-deck-list}}
    :view-data {:selected-deck nil}})
