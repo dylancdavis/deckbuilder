f0eeb390e72efd862325b321dccbe575139933b7
dylancdavis <dylcdav@gmail.com>
2024-04-06 19:40:31 -0400

    Add rules-card-content

--- src/deckbuilder/views/cards.cljs
+++ src/deckbuilder/views/cards.cljs
@@ -1,9 +1,18 @@
 (ns deckbuilder.views.cards (:require [deckbuilder.views.svg :as svg]))
 
+(defn playable-card-content [card]
+  [:<> [:div.card-image svg/lightning-svg]
+   [:div.card-description (:description card)]])
+
+(defn rules-card-content [card]
+  [:div.rules-info
+   [:div.card-description "Hello"]])
+
 (defn card-item [card]
   [:div.card-container {:key (js/Math.random)}
    [:div.card-background
     [:div.card-name (:name card)]
     [:div.card-content
-     [:div.card-image svg/lightning-svg]
-     [:div.card-description (:description card)]]]])
+     (case (:type card)
+       :rules (rules-card-content card)
+       (playable-card-content card))]]])
