05b4cc18b6d6a9af88bee4e0cd954d05ef40248b
Cole D <104338788+dylancdavis@users.noreply.github.com>
2024-06-03 17:28:08 +0000

    Add more card definitions

--- src/deckbuilder/game/cards.cljs
+++ src/deckbuilder/game/cards.cljs
@@ -18,6 +18,43 @@
                   :end-conditions {:rounds 1}
                   :cost 2})
 
+(def dual-score {:name "Dual Score"
+                 :description "Gain 2 Points. Deck Limit 2."
+                 :deck-limit 2
+                 :event [:gain-resource :points 2]
+                 :cost 4})
+
+(def save-reward {:name "A Penny Saved"
+                   :description "If you haven't purchased a card this round, gain 2 points."
+                   :cost 4})
+
+(def zero-reward {:name "Starting Surge"
+                   :description "If you have 0 points, gain 6 points."
+                   :cost 4})
+
+(def point-reset {:name "Point Reboot"
+                  :description "Lose all points, then gain 4 points."
+                  :cost 6})
+
+(def point-multiply {:name "Point Multiplication"
+                     :description "If you have 4 or less points, double them."})
+
+(def score-surge {:name "Score Surge"
+                   :description "Gain 2 points for each \"Score\"played this round (up to 4)."
+                   :cost 10})
+
+(def score-synergy {:name "Score Synergy"
+                     :description "Gain 1 point for each \"Score\" in your deck (up to 6)."
+                    :cost 10})
+
+(def borrow-points {:name "Borrowed Points"
+                     :description "Gain 6 points. In 2 turns, lose 6 points (down to zero)"
+                     :cost 10})
+
+(def last-resort {:name "Last Resort"
+                   :description "Gain 8 Points, then destroy this card"
+                   :cost 12})
+
 (def basic-cards #{score buy-basic basic-rules})
 
 (def starting-deck-list (cons buy-basic (take 7 (repeat score))))
