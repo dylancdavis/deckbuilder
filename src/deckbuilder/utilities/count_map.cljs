fb20993f02dd5042424915a06dc3234f5809e8f1
dylancdavis <dylcdav@gmail.com>
2024-07-04 17:18:51 -0400

    Add more countmap utilities

--- src/deckbuilder/utilities/count_map.cljs
+++ src/deckbuilder/utilities/count_map.cljs
@@ -1,9 +1,16 @@
 (ns deckbuilder.utilities.count-map)
 
-(defn inc-in-map [map key] (if (nil? (get map key)) (assoc map key 1) (update-in map [key] inc)))
+(defn inc-in-map
+  ([map key n] (if (nil? (get map key)) (assoc map key n) (update-in map [key] #(+ % n))))
+  ([map key] (inc-in-map map key 1)))
 
-(defn dec-in-map [map key]
-  (cond
-    (nil? (get map key)) map
-    (= 1 (get map key)) (dissoc map key)
-    :else (update-in map [key] dec)))
+(defn dec-in-map
+  ([map key n] (cond
+                 (nil? (get map key)) map
+                 (<= n (get map key)) (dissoc map key)
+                 :else (update-in map [key] #(- % n))))
+  ([map key] (dec-in-map map key 1)))
+
+(defn merge-count-maps [m1 m2] (reduce (fn [acc [k v]] (inc-in-map acc k v)) m1 m2))
+
+(defn to-seq [map] (mapcat (fn [[k v]] (repeat v k)) map))
