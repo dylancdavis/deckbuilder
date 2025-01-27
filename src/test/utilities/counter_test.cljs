1980cf6bf20448a34123c360f59601b53866490f
dylancdavis <dylcdav@gmail.com>
2025-01-27 15:54:42 -0500

    Update total tests

--- src/test/utilities/counter_test.cljs
+++ src/test/utilities/counter_test.cljs
@@ -1,8 +1,8 @@
 (ns test.utilities.counter-test
   (:require [cljs.test :refer [deftest testing is]]
-            [deckbuilder.utilities.counter :refer [add sub merge-counters]]))
+            [deckbuilder.utilities.counter :refer [add sub total merge-counters]]))
 
-(deftest counter 
+(deftest counter
   (testing "Counter"
     (testing "adds correctly"
       (is (= (add {} :a) {:a 1}) "Adding to empty counter")
@@ -12,7 +12,11 @@
       (is (= (sub {:a 1} :a) {}) "Subtracting to zero doesn't yield empty counter")
       (is (= (sub {:a 2} :a) {:a 1}) "Subtracting from existing key")
       (is (= (sub {:a 2} :b) {:a 2}) "Subtracting non-existing key")
-      (is (= (sub {:a 2} :a 3) {}) "Subtracting more than available")
-      (testing "merges correctly"
-        (is (= (merge-counters {:a 1} {:b 2}) {:a 1 :b 2}) "Merging two counters")
-        (is (= (merge-counters {:a 1} {:a 2}) {:a 3}))))) "Merging two counters with same key")
+      (is (= (sub {:a 2} :a 3) {}) "Subtracting more than available"))
+    (testing "totals correctly"
+      (is (= (total {}) 0) "Total of empty counter")
+      (is (= (total {:a 1}) 1) "Total of one key")
+      (is (= (total {:a 1 :b 2 :c 3}) 6) "Total of multiple keys"))
+    (testing "merges correctly"
+      (is (= (merge-counters {:a 1} {:b 2}) {:a 1 :b 2}) "Merging two counters")
+      (is (= (merge-counters {:a 1} {:a 2}) {:a 3}) "Merging two counters with same key"))))
