f8069bd347583de65409e70bbb5cf878c6f2e1d2
dylancdavis <dylcdav@gmail.com>
2025-01-27 14:11:44 -0500

    Add new-deck-name-test

---
+++ src/test/utilities/collection_test.cljs
@@ -0,0 +1,9 @@
+(ns test.utilities.collection-test
+  (:require [cljs.test :refer [deftest is]]
+            [deckbuilder.utilities.collection :refer [new-deck-name]]))
+
+(deftest new-deck-name-test
+  (is (= (new-deck-name []) "New Deck 1") "New deck from empty list isn't named \"New Deck 1\"")
+    (is (= (new-deck-name ["New Deck 1"]) "New Deck 2") "Deck name isn't incremented")
+    (is (= (new-deck-name ["New Deck 1" "New Deck 3"]) "New Deck 2") "Missing deck number isn't reused")
+    (is (= (new-deck-name ["NewDeck1"]) "New Deck 1") "Deck name isn't \"New Deck 1\" with non-new name in list"))
