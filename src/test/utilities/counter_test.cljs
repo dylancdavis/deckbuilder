bf24c63b1fafd68edee64d4eb3a05b5acc314955
dylancdavis <dylcdav@gmail.com>
2025-01-27 14:14:52 -0500

    Minor test messages

--- src/test/utilities/counter_test.cljs
+++ src/test/utilities/counter_test.cljs
@@ -5,14 +5,14 @@
 (deftest counter 
   (testing "Counter"
     (testing "adds correctly"
-      (is (= (add {} :a) {:a 1}))
-      (is (= (add {:a 1} :a) {:a 2}))
-      (is (= (add {:a 1} :b) {:a 1 :b 1})))
+      (is (= (add {} :a) {:a 1}) "Adding to empty counter")
+      (is (= (add {:a 1} :a) {:a 2}) "Adding to existing key")
+      (is (= (add {:a 1} :b) {:a 1 :b 1})) "Adding new key to existing counter")
     (testing "subtracts correctly"
-      (is (= (sub {:a 1} :a) {}))
-      (is (= (sub {:a 2} :a) {:a 1}))
-      (is (= (sub {:a 2} :b) {:a 2}))
-      (is (= (sub {:a 2} :a 3) {})))
-    (testing "merges correctly"
-      (is (= (merge-counters {:a 1} {:b 2}) {:a 1 :b 2}))
-      (is (= (merge-counters {:a 1} {:a 2}) {:a 3})))))
+      (is (= (sub {:a 1} :a) {}) "Subtracting to zero doesn't yield empty counter")
+      (is (= (sub {:a 2} :a) {:a 1}) "Subtracting from existing key")
+      (is (= (sub {:a 2} :b) {:a 2}) "Subtracting non-existing key")
+      (is (= (sub {:a 2} :a 3) {}) "Subtracting more than available")
+      (testing "merges correctly"
+        (is (= (merge-counters {:a 1} {:b 2}) {:a 1 :b 2}) "Merging two counters")
+        (is (= (merge-counters {:a 1} {:a 2}) {:a 3}))))) "Merging two counters with same key")
--- src/test/utilities/utils_test.cljs
+++ src/test/utilities/utils_test.cljs
@@ -3,8 +3,8 @@
             [deckbuilder.utilities.utils :refer [first-missing-num]]))
 
 (deftest first-missing-num-test
-  (is (= (first-missing-num []) 1))
-  (is (= (first-missing-num [1 2 3]) 4))
-  (is (= (first-missing-num [1 3 4]) 2))
-  (is (= (first-missing-num [1 2 3 5 7]) 4))
-  (is (= (first-missing-num [2 3 5]) 1)))
+  (is (= (first-missing-num []) 1) "Empty list should return 1")
+  (is (= (first-missing-num [1 2 3]) 4) "No missing numbers should return next number")
+  (is (= (first-missing-num [1 3 4]) 2) "Missing number should return that number")
+  (is (= (first-missing-num [1 2 3 5 7]) 4) "Multiple missing numbers should return first one")
+  (is (= (first-missing-num [2 3 5]) 1)) "If 1 is missing, it should be returned")
