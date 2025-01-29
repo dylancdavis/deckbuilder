9613bd3269cf6c083f8156d2d3d5a5893c4ce9e3
dylancdavis <dylcdav@gmail.com>
2025-01-29 14:11:18 -0500

    Extract deck-in-size-range? to own function from check-deck-validity

--- src/deckbuilder/game/deck.cljs
+++ src/deckbuilder/game/deck.cljs
@@ -1,7 +1,17 @@
 (ns deckbuilder.game.deck
   (:require [deckbuilder.utilities.counter :refer [total]]))
 
-(defn check-deck-validity [deck collection]
+(defn deck-in-size-range? "Returns true if a `deck` has a size within the range specified by its rules card." [deck]
   (let [deck-size (total (:cards deck))
-        [min-size max-size] (get-in deck [:rules-card :deck-limits :size])]
-    (<= min-size deck-size max-size)))
+        rules-card (:rules-card deck)
+        [min-size max-size] (get-in rules-card [:deck-limits :size])]
+    (if (nil? rules-card) true (<= min-size deck-size max-size))))
+
+(def deck-checks {:in-size-range deck-in-size-range?})
+
+(defn check-deck-validity "Given a `deck`, a `collection` and a `checks` list of keyword-predicate pairs, returns a map for whether each check was passed."
+  [deck collection]
+  (reduce (fn [acc [check-key check-fn]]
+            (assoc acc check-key (check-fn deck collection)))
+          {}
+          deck-checks))
--- test/deckbuilder/game/deck_test.cljs
+++ test/deckbuilder/game/deck_test.cljs
@@ -1,12 +1,14 @@
 (ns deckbuilder.game.deck-test
   (:require [cljs.test :refer [deftest is]]
-            [deckbuilder.game.deck :refer [check-deck-validity]]))
+            [deckbuilder.game.deck :refer [deck-in-size-range?]]))
 
-(deftest check-deck-validity-test
-  (is (= (check-deck-validity {:cards {} :rules-card {:deck-limits {:size [0,0]}}} {}) true) "Empty deck should be valid for [0,0]")
-  (is (= (check-deck-validity {:cards {:a 3} :rules-card {:deck-limits {:size [4,8]}}} {}) false) "Deck below minimmum size shoulve be invalid")
-  (is (= (check-deck-validity {:cards {:a 9} :rules-card {:deck-limits {:size [4,8]}}} {}) false) "Deck above maximum size should be invalid")
-  (is (= (check-deck-validity {:cards {:a 4} :rules-card {:deck-limits {:size [4,8]}}} {}) true) "Deck with minimum size should be valid")
-  (is (= (check-deck-validity {:cards {:a 6} :rules-card {:deck-limits {:size [4,8]}}} {}) true) "Deck between range should be valid")
-  (is (= (check-deck-validity {:cards {:a 8} :rules-card {:deck-limits {:size [4,8]}}} {}) true) "Deck with maximum size should be valid"))
+(deftest deck-in-size-range?-test
+  (is (= (deck-in-size-range? {:cards {} :rules-card nil}) true) "Empty deck should be vacuously true when no rules card")
+  (is (= (deck-in-size-range? {:cards {:a 3} :rules-card nil}) true) "Populated deck should be vacuously true when no rules card")
+  (is (= (deck-in-size-range? {:cards {} :rules-card {:deck-limits {:size [0,0]}}}) true) "Empty deck should be valid for [0,0]")
+  (is (= (deck-in-size-range? {:cards {:a 3} :rules-card {:deck-limits {:size [4,8]}}}) false) "Deck below minimmum size shoulve be invalid")
+  (is (= (deck-in-size-range? {:cards {:a 9} :rules-card {:deck-limits {:size [4,8]}}}) false) "Deck above maximum size should be invalid")
+  (is (= (deck-in-size-range? {:cards {:a 4} :rules-card {:deck-limits {:size [4,8]}}}) true) "Deck with minimum size should be valid")
+  (is (= (deck-in-size-range? {:cards {:a 6} :rules-card {:deck-limits {:size [4,8]}}}) true) "Deck between range should be valid")
+  (is (= (deck-in-size-range? {:cards {:a 8} :rules-card {:deck-limits {:size [4,8]}}}) true) "Deck with maximum size should be valid"))
   
