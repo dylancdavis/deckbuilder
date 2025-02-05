3bc37393d752df92a35ab3ed5d805e7d283516e4
dylancdavis <dylcdav@gmail.com>
2025-02-05 11:31:38 -0500

    Minor formatting & more tests for utilities

--- src/deckbuilder/utilities/utils.cljs
+++ src/deckbuilder/utilities/utils.cljs
@@ -7,12 +7,14 @@
         missing-nums (set/difference potential-missing-nums (set xs))]
     (apply min missing-nums)))
 
-(defn select-keys-by "Returns a map containing only those entries in map whose key has a value that satisfies a predicate"
+(defn select-keys-by 
+  "Returns a map containing only those entries in map whose key has a value that satisfies a predicate"
   [m pred]
   (let [selected-keys (filter #(pred (second %)) m)]
     (select-keys m (map first selected-keys))))
 
-(defn move-items "Transfers the first `amount` items from the first sequence to the second, returning the updated sequence."
+(defn move-items 
+  "Transfers the first `amount` items from the first sequence to the second, returning the updated sequence."
   [from-seq to-seq amount]
   (let [items-to-move (take amount from-seq)]
     [(drop amount from-seq) (concat to-seq items-to-move)]))
--- test/deckbuilder/utilities/utils_test.cljs
+++ test/deckbuilder/utilities/utils_test.cljs
@@ -1,10 +1,20 @@
 (ns deckbuilder.utilities.utils-test
   (:require [cljs.test :refer [deftest is]]
-            [deckbuilder.utilities.utils :refer [first-missing-num]]))
+            [deckbuilder.utilities.utils :refer [first-missing-num select-keys-by move-items]]))
 
 (deftest first-missing-num-test
-  (is (= (first-missing-num []) 1) "Empty list should return 1")
-  (is (= (first-missing-num [1 2 3]) 4) "No missing numbers should return next number")
-  (is (= (first-missing-num [1 3 4]) 2) "Missing number should return that number")
-  (is (= (first-missing-num [1 2 3 5 7]) 4) "Multiple missing numbers should return first one")
-  (is (= (first-missing-num [2 3 5]) 1)) "If 1 is missing, it should be returned")
+  (is (= (first-missing-num []) 1) "Should return 1 when given empty list")
+  (is (= (first-missing-num [1 2 3]) 4) "Should return next number for a standard sequence")
+  (is (= (first-missing-num [1 3 4]) 2) "Should return a skipped number in the middle of the sequence")
+  (is (= (first-missing-num [1 2 3 5 7]) 4) "Should return the first misssing number when multiple are missing")
+  (is (= (first-missing-num [2 3 5]) 1)) "Should return 1 if it's missing from the sequence")
+
+(deftest select-keys-by-test
+  (is (= (select-keys-by {:a 1 :b 2 :c 3} odd?) {:a 1 :c 3}) "Should filter to only keys with odd values")
+  (is (= (select-keys-by {:a 1 :b 2 :c 3} even?) {:b 2}) "Should filter to only keys with even values")
+  (is (= (select-keys-by {:a 1 :b 2 :c 3} #(> % 1)) {:b 2 :c 3}) "Should filter to only keys with values greater than 1"))
+
+(deftest move-items-test
+  (is (= (move-items [1 2 3 4] [5 6 7 8] 2) [[3 4] [5 6 7 8 1 2]]) "Should move 2 items from first to second")
+  (is (= (move-items [1 2 3 4] [5 6 7 8] 4) [[] [5 6 7 8 1 2 3 4]]) "Should move all items from first to second")
+  (is (= (move-items [1 2 3 4] [5 6 7 8] 0) [[1 2 3 4] [5 6 7 8]]) "Should move 0 items from first to second"))
