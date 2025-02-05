45a0e72f299b7b94bb58417ebc72f6c41e83c16c
dylancdavis <dylcdav@gmail.com>
2025-02-05 11:37:15 -0500

    Slightly better tests for utilities

--- test/deckbuilder/utilities/utils_test.cljs
+++ test/deckbuilder/utilities/utils_test.cljs
@@ -10,11 +10,13 @@
   (is (= (first-missing-num [2 3 5]) 1)) "Should return 1 if it's missing from the sequence")
 
 (deftest select-keys-by-test
-  (is (= (select-keys-by {:a 1 :b 2 :c 3} odd?) {:a 1 :c 3}) "Should filter to only keys with odd values")
-  (is (= (select-keys-by {:a 1 :b 2 :c 3} even?) {:b 2}) "Should filter to only keys with even values")
-  (is (= (select-keys-by {:a 1 :b 2 :c 3} #(> % 1)) {:b 2 :c 3}) "Should filter to only keys with values greater than 1"))
+  (is (= (select-keys-by {} odd?) {}) "Should return an empty map in base case")
+  (is (= (select-keys-by {:a 1 :b 3 :c 5} even?) {}) "Should return empty map when no values match")
+  (is (= (select-keys-by {:a 1 :b 3 :c 5} odd?) {:a 1 :b 3 :c 5}) "Should return the same map when all values match")
+  (is (= (select-keys-by {:a 1 :b 2 :c 3} odd?) {:a 1 :c 3}) "Should filter to only keys according to predicate"))
 
 (deftest move-items-test
   (is (= (move-items [1 2 3 4] [5 6 7 8] 2) [[3 4] [5 6 7 8 1 2]]) "Should move 2 items from first to second")
   (is (= (move-items [1 2 3 4] [5 6 7 8] 4) [[] [5 6 7 8 1 2 3 4]]) "Should move all items from first to second")
-  (is (= (move-items [1 2 3 4] [5 6 7 8] 0) [[1 2 3 4] [5 6 7 8]]) "Should move 0 items from first to second"))
+  (is (= (move-items [1 2 3 4] [5 6 7 8] 0) [[1 2 3 4] [5 6 7 8]]) "Should move 0 items from first to second")
+  (is (= (move-items [1 2 3 4] [] 4) [[] [1 2 3 4]]) "Should move items into an empty vector"))
