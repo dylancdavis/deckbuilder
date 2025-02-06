fe6799190be83a17c138f5f21b3ae93bb33ee4d7
dylancdavis <dylcdav@gmail.com>
2025-02-06 08:41:02 -0500

    Add insert-randomly, into-randomly

--- src/deckbuilder/utilities/utils.cljs
+++ src/deckbuilder/utilities/utils.cljs
@@ -7,14 +7,27 @@
         missing-nums (set/difference potential-missing-nums (set xs))]
     (apply min missing-nums)))
 
-(defn select-keys-by 
+(defn select-keys-by
   "Returns a map containing only those entries in map whose key has a value that satisfies a predicate"
   [m pred]
   (let [selected-keys (filter #(pred (second %)) m)]
     (select-keys m (map first selected-keys))))
 
-(defn move-items 
+(defn move-items
   "Transfers the first `amount` items from the first sequence to the second, returning the updated sequence."
   [from-seq to-seq amount]
   (let [items-to-move (take amount from-seq)]
     [(drop amount from-seq) (concat to-seq items-to-move)]))
+
+(defn insert-randomly
+  "Inserts `item` into `coll` at a random index."
+  [coll item]
+  (let [index (rand-int (inc (count coll)))]
+    (concat (take index coll) [item] (drop index coll))))
+
+(defn into-randomly 
+  "Inserts `items` into `coll` at a random index, one at a time."
+  [coll items]
+  (if (empty? items)
+    coll
+    (recur (insert-randomly coll (first items)) (rest items))))
