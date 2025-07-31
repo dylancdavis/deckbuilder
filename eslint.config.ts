7bc3b8604167f3f2bb28ca49bf3d64d11a5df1b1
Cole Davis <dylcdav@gmail.com>
2025-07-30 20:55:41 -0400

    Exlcude _ from no-unused-vars

--- eslint.config.ts
+++ eslint.config.ts
@@ -13,13 +13,19 @@ export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
