65666bc519b9fa12f810148ddaa954fec1366990
Cole Davis <dylcdav@gmail.com>
2025-09-13 16:30:02 -0400

    Add CSS to stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,12 @@
 import type { Preview } from '@storybook/vue3-vite'
 
+// Import CSS files to make components look correct
+import '../src/assets/css/reset.css'
+import '../src/assets/css/index.css'
+import '../src/assets/css/cards.css'
+import '../src/assets/css/collection.css'
+import '../src/assets/css/run.css'
+
 const preview: Preview = {
   parameters: {
     controls: {
