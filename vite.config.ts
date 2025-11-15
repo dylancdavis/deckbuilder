eca6fb60ffaa9a06bd046e4ee00e1bfec63046a2
Cole D <104338788+dylancdavis@users.noreply.github.com>
2025-11-15 01:19:03 -0500

    Conditionally enable Vue devtools plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
+const plugins: PluginOption[] = [vue()]
+
+if (process.env.NODE_ENV !== 'production') {
+  plugins.push(vueDevTools())
+}
+
 export default defineConfig({
   base: '/deckbuilder/',
   // vueDevTools() is currently incompatibile with Storybook.
   // Comment it out if Storybook is crashing.
-  plugins: [vue(), vueDevTools()],
+  plugins,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
